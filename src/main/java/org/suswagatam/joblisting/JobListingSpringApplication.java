4559a988981a7f7c3a3298c4763c4a14e7235c7e
Suswagatam Rong <suswagatam_rong@protonmail.com>
2024-02-07 13:39:24 +0530

    initialised the project, added Swagger dependencies'

---
+++ src/main/java/org/suswagatam/joblisting/JobListingSpringApplication.java
@@ -0,0 +1,13 @@
+package org.suswagatam.joblisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobListingSpringApplication.class, args);
+    }
+
+}
